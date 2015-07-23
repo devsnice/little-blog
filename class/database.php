a96401d4b497bc46e5651c00f4175cc4e4781f27
devsnice <devsnice@gmail.com>
2015-07-23 15:33:42 +0800

    Added consruct for class than work with DataBase

---
+++ class/database.php
@@ -0,0 +1,52 @@
+<?php
+
+// Class form work with DataBase
+
+class db {
+    private $db = "blog";
+    private $login  = "root";
+    private $pass   = "";
+    private $link;
+
+    // Connect to db
+    function __construct() {
+        mysql_connect("localhost", $this->login, $this->pass);
+        mysql_select_db($this->db);
+    }
+
+    // Disconnect fron db
+    function disconnect() {
+        mysql_close($this->link);
+    }
+
+    // Insert data in db
+    function insert($query) {
+        $result = mysql_query($query);
+        return $result; // true or false
+    }
+
+    // Select data from db
+    function select($query) {
+        $result = [];
+
+        $answer = mysql_query($query);
+
+        while($res = mysql_fetch_assoc($answer)) {
+            $result[] = $res;
+        }
+
+        return $result; //array of element;
+    }
+
+    // Update data in db
+    function upgrade($query) {
+        $result = mysql_query($query);
+    }
+
+    // Delete data from db
+    function delete($query) {
+        $result = mysql_query($query);
+    }
+
+
+}
