ef8dc85fa765faa8cc9ce2cc6310011505401e46
devsnice <devsnice@gmail.com>
2015-08-01 18:05:39 +0800

    Edit Index.php - now it's a controller for page

--- index.php
+++ index.php
@@ -1,106 +1,14 @@
-<?php include '/blocks/header.php'; ?>
-<?php include '/blocks/navigation.php'; ?>
+<?php
+    require __DIR__ . "/class/database.php";
+    require __DIR__ . "/models/article.php";
 
-<main class="main" role="main">
-    <div class="wrapper clearfix">
-        <!-- All articles -->
-        <section class="main-articles">
-            <article class='article article_main'>
-                <header class='article-header clearfix'>
-                    <h2 class='article__title'>
-                        <a href='#'>This's testing!</a>
-                    </h2>
+    // Create to connect for db
+    $db = new db("articleShort");
+    $modelArticle = new modelArticle($db);
 
-                    <div class='article__categories'>
-                        <a href='#'>Coding</a>
-                        <a href='#'>Live</a>
-                    </div>
-                </header>
+    // Get all article from DataBase
+    $articles = $modelArticle->getAllArticle();
 
-                <p>Software testing is an investigation conducted to provide stakeholders with information about the quality of the product or service under test.[1] Software testing can also provide an objective, independent view of the software to allow the business to appreciate and understand the risks of software implementation. Test techniques include the process of executing a program or application with the intent of finding software bugs (errors or other defects).</p>
-
-                <p>
-                    Software testing can be conducted as soon as executable software (even if partially complete) exists. The overall approach to software development often determines when and how testing is conducted. For example, in a phased process, most testing occurs after system requirements have been defined and then implemented in testable programs. In contrast, under an Agile approach, requirements, programming, and testing are often done concurrently.
-                </p>
-                <footer class='article-footer'>
-                    <span class='i-bold'>Add: </span> 21.07.2015
-                </footer>
-            </article>
-
-            <article class='article article_main article_main_half'>
-                <header class='article-header clearfix'>
-                    <h2 class='article__title'>
-                        <a href='#'>This's testing!</a>
-                    </h2>
-
-                    <div class='article__categories'>
-                        <a href='#'>Coding</a>
-                        <a href='#'>Live</a>
-                    </div>
-                </header>
-
-                <p>Software testing is an investigation conducted to provide stakeholders with information about the quality of the product or service under test.[1] Software testing can also provide an objective, independent view of the software to allow the business to appreciate and understand the risks of software implementation. Test techniques include the process of executing a program or application with the intent of finding software bugs (errors or other defects).</p>
-
-                <p>
-                    Software testing can be conducted as soon as executable software (even if partially complete) exists. The overall approach to software development often determines when and how testing is conducted. For example, in a phased process, most testing occurs after system requirements have been defined and then implemented in testable programs. In contrast, under an Agile approach, requirements, programming, and testing are often done concurrently.
-                </p>
-                <footer class='article-footer'>
-                    <span class='i-bold'>Add: </span> 21.07.2015
-                </footer>
-            </article>
-
-            <article class='article article_main article_main_half' >
-                <header class='article-header clearfix'>
-                    <h2 class='article__title'>
-                        <a href='#'>This's testing!</a>
-                    </h2>
-
-                    <div class='article__categories'>
-                        <a href='#'>Coding</a>
-                        <a href='#'>Live</a>
-                    </div>
-                </header>
-
-                <p>Software testing is an investigation conducted to provide stakeholders with information about the quality of the product or service under test.[1] Software testing can also provide an objective, independent view of the software to allow the business to appreciate and understand the risks of software implementation. Test techniques include the process of executing a program or application with the intent of finding software bugs (errors or other defects).</p>
-
-                <p>
-                    Software testing can be conducted as soon as executable software (even if partially complete) exists. The overall approach to software development often determines when and how testing is conducted. For example, in a phased process, most testing occurs after system requirements have been defined and then implemented in testable programs. In contrast, under an Agile approach, requirements, programming, and testing are often done concurrently.
-                </p>
-                <footer class='article-footer'>
-                    <span class='i-bold'>Add: </span> 21.07.2015
-                </footer>
-            </article>
-
-
-            <article class='article article_main'>
-                <header class='article-header clearfix'>
-                    <h2 class='article__title'>
-                        <a href='#'>This's testing!</a>
-                    </h2>
-
-                    <div class='article__categories'>
-                        <a href='#'>Coding</a>
-                        <a href='#'>Live</a>
-                    </div>
-                </header>
-
-                <p>Software testing is an investigation conducted to provide stakeholders with information about the quality of the product or service under test.[1] Software testing can also provide an objective, independent view of the software to allow the business to appreciate and understand the risks of software implementation. Test techniques include the process of executing a program or application with the intent of finding software bugs (errors or other defects).</p>
-
-                <p>
-                    Software testing can be conducted as soon as executable software (even if partially complete) exists. The overall approach to software development often determines when and how testing is conducted. For example, in a phased process, most testing occurs after system requirements have been defined and then implemented in testable programs. In contrast, under an Agile approach, requirements, programming, and testing are often done concurrently.
-                </p>
-                <footer class='article-footer'>
-                    <span class='i-bold'>Add: </span> 21.07.2015
-                </footer>
-            </article>
-        </section>
-
-        <!-- Navigation by category -->
-
-    </div>
-</main>
-
-<?php include 'blocks/footer.php'; ?>
-
-</body>
-</html>
+    // View articles
+    include __DIR__ . "/views/index.php";
+?>
